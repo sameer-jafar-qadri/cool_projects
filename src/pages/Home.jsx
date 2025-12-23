00dcf097d47009fe483aed0e331744f6f0620d46
sameer-jafar-qadri <sameer.jafar.qadri@gmail.com>
2025-12-23 06:58:00 +0530

    header at right position

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,18 +6,22 @@ export default function Home() {
     const navigate = useNavigate();
 
     return (
-        <div className="min-h-screen flex flex-col items-center justify-center bg-linear-to-r from-purple-400 via-pink-400 to-red-400 text-white">
-            <Header/>
-            <h1 className="text-4xl font-bold mb-6">Welcome to the Utility App</h1>
-            <p className="mb-6 text-center text-lg max-w-md">
-                Click the button below to go to the Random Number Generator.
-            </p>
-            <button
-                onClick={() => navigate("/random")}
-                className="px-6 py-3 bg-white text-purple-600 font-semibold rounded-xl shadow-lg hover:scale-105 transform transition"
-            >
-                Go to Random Number Generator
-            </button>
-        </div>
-    );
+        <>
+        <Header/>
+            <div className="min-h-screen flex flex-col items-center justify-center bg-linear-to-r from-purple-400 via-pink-400 to-red-400 text-white">
+
+                <h1 className="text-4xl font-bold mb-6">Welcome to the Utility App</h1>
+                <p className="mb-6 text-center text-lg max-w-md">
+                    Click the button below to go to the Random Number Generator.
+                </p>
+                <button
+                    onClick={() => navigate("/random")}
+                    className="px-6 py-3 bg-white text-purple-600 font-semibold rounded-xl shadow-lg hover:scale-105 transform transition"
+                >
+                    Go to Random Number Generator
+                </button>
+            </div>
+
+        </>
+            );
 }
